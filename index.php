1740378243f65e2794322b21ba777f2f858f863b
hagrid862 <n.okroj@icloud.com>
2024-12-04 12:55:57 +0100

    Dodano wyświetlanie danych z bazy danych jako tabela PHP

--- index.php
+++ index.php
@@ -24,13 +24,42 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+      table, th, td {
+        border: 1px solid black;
+      }
+
+      th, td {
+        background-color: #fff;
+        padding: 6px 12px;
+      }
+
+      table {
+        background-color: #000;
+      }
+    </style>
 </head>
 <body>
     <h1>здраствуй цие</h1>
+    <table>
+      <tr>
+        <th>Id</th>
+        <th>Imie</th>
+        <th>Nazwisko</th>
+        <th>Rok Urodzenia</th>
+        <th>Miejsce Urodzenia</th>
+      </tr>
     <?php
     while ($row = $result->fetch_assoc()) {
-        echo "<p>" . $row["Imie"] . " " . $row["Nazwisko"] . "</p>";
+        echo "<tr>";
+        echo "<td>" . $row["Id"] . "</td>";
+        echo "<td>" . $row["Imie"] . "</td>";
+        echo "<td>" . $row["Nazwisko"] . "</td>";
+        echo "<td>" . $row["Rok_urodzenia"] . "</td>";
+        echo "<td>" . $row["Miejsce_urodzenia"] . "</td>";
+        echo "</tr>";
     }
     ?>
+    </table>
 </body>
 </html>
