3b8145e641b2930d8132a4393de3d63d43d45586
Nikodem Okroj <n.okroj@icloud.com>
2025-01-08 12:56:52 +0100

    Dodano wyswietlanie ankiet

--- index.php
+++ index.php
@@ -3,6 +3,30 @@ $database_adress="db";
 $database_user="root";
 $database_password="supersecret";
 $database_db="baza";
+
+function getOdpCount($database_adress, $database_user, $database_password, $database_db, $id_ankiety) {
+    $conn = new mysqli($database_adress, $database_user, $database_password, $database_db);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM odpowiedz WHERE ankieta_id = ".$id_ankiety.";";
+    $res = $conn->query($sql);
+    return $res->num_rows;
+}
+
+function getGlosCount($database_adress, $database_user, $database_password, $database_db, $id_ankiety) {
+    $conn = new mysqli($database_adress, $database_user, $database_password, $database_db);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM glos WHERE ankieta_id = ".$id_ankiety.";";
+    $res = $conn->query($sql);
+    return $res->num_rows;
+}
 ?>
 
 <!DOCTYPE html>
@@ -11,17 +35,51 @@ $database_db="baza";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./style.css"/>
 </head>
 <body>
-    <h1>Dostepe ankiety:</h1>
-    <?php
-        $conn = new mysqli($database_adress, $database_user, $database_password);
+    <div class="main">
+        <h1>Dostepe ankiety:</h1>
+        <?php
+            $conn = new mysqli($database_adress, $database_user, $database_password, $database_db);
 
-        if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
-        }
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
 
-        echo "connected successfully";
-    ?>
+            $sql = "SELECT * FROM ankieta;";
+
+            $res = $conn->query($sql);
+
+            if ($res->num_rows > 0) {
+                while ($row = $res->fetch_assoc()) {
+                    echo '
+                        <div class="card">
+                            <div class="card-title">'.
+                                $row["pytanie"].'
+                            </div>
+                            <div class="card-odp-count">
+                                Ilosc dostepnych odpowiedzi: '.
+                                getOdpCount($database_adress, $database_user, $database_password, $database_db, $row["id"])
+                            .'
+                            </div>
+                            <div class="card-odp-count">
+                                Ilosc oddanych glosow: '.
+                                getGlosCount($database_adress, $database_user, $database_password, $database_db, $row["id"])
+                            .'
+                            </div>
+                            <button class="glosuj-button">
+                                zaglosuj
+                            </button>
+                        </div>
+                    ';
+                }
+            } else {
+                echo "brak ankiet";
+            }
+
+
+        ?>
+    </div>
 </body>
 </html>
