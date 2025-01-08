b68a0f551752224922739702300eb5440fa682b7
Nikodem Okroj <n.okroj@icloud.com>
2025-01-08 12:12:48 +0100

    Dodano skrypty do tworzenia tabel bazy danych

---
+++ baza.sql
@@ -0,0 +1,25 @@
+CREATE TABLE ankieta (
+    id INT NOT NULL AUTO_INCREMENT,
+    pytanie VARCHAR(128) NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE odpowiedz (
+    id INT NOT NULL AUTO_INCREMENT,
+    tresc VARCHAR(128) NOT NULL,
+    ankieta_id INT NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (ankieta_id) REFERENCES ankieta(id)
+);
+
+CREATE TABLE glos (
+    id INT NOT NULL AUTO_INCREMENT,
+    ankieta_id INT NOT NULL,
+    odpowiedz_id INT NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (ankieta_id) REFERENCES ankieta(id),
+    FOREIGN KEY (odpowiedz_id) REFERENCES odpowiedz(id)
+);
