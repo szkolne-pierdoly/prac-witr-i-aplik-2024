07973f43957ac8d3ae4365c17364d99dd3b8f294
Nikodem Okroj <n.okroj@icloud.com>
2025-01-15 12:53:25 +0100

    Dodano kwerende druga

--- index.php
+++ index.php
@@ -22,48 +22,52 @@
             <input type="submit" value="Filmy" />
         </form>
         <?php
-            if (!isset($_POST['gat'])) {
-                return;
-            }
-
             try {
                 $connection = new mysqli("db", "root", "supersecret", "baza_15_01_2025", 3306);
                 if ($connection->connect_error) {
                     throw new Exception("Connection failed: " . $connection->connect_error);
                 }
+                #zapytanie 1 - wykonywane tylko po wybraniu gatunku
 
-                $gatunek = $_POST['gat'] == 'zse' ? '3' : $_POST['gat'];
+                if (isset($_POST['gat'])) {
+                    $gatunek = $_POST['gat'] == 'zse' ? '3' : $_POST['gat'];
 
-                $sql = "SELECT tytul, rok, ocena FROM filmy WHERE gatunki_id = ".$gatunek.";";
+                    $sql = "SELECT tytul, rok, ocena FROM filmy WHERE gatunki_id = ".$gatunek.";";
+    
+                    $res = $connection->query($sql);
+    
+                    $gatslownie = "";
+    
+                    
+                    if ($gatunek == "1") {
+                            $gatslownie = "Sci-Fi";
+                    } else if ($gatunek == "2") {
+                            $gatslownie = "animacja";
+                    } else if ($gatunek == "3") {
+                            $gatslownie = "dramat";
+                    } else if ($gatunek == "4") {
+                            $gatslownie = "horror";
+                    } else if ($gatunek == "5") {
+                        $gatslownie = "komedia";
+                    }
+                    
+                    echo "<h2>".$gatunek.". ".$gatslownie."</h2>";
+    
+                    if ($res->num_rows > 0) {
+                        while ($row = $res->fetch_assoc()) {
+                            echo "<p>tytul: ".$row['tytul']."; Rok produkcji: ".$row['rok']."; ocena: ".$row['ocena']."</p>";
+                        }
+                    }
+                }
+                $sql = "SELECT filmy.id, filmy.tytul, (SELECT rezyserzy.imie FROM rezyserzy WHERE rezyserzy.id = rezyserzy_id) AS imie_rezysera, (SELECT rezyserzy.nazwisko FROM rezyserzy WHERE rezyserzy.id = rezyserzy_id) AS nazwisko_rezysera FROM filmy;";
 
                 $res = $connection->query($sql);
 
-                $gatslownie = "";
-
-                
-                if ($gatunek == "1") {
-                        $gatslownie = "Sci-Fi";
-                } else if ($gatunek == "2") {
-                        $gatslownie = "animacja";
-                } else if ($gatunek == "3") {
-                        $gatslownie = "dramat";
-                } else if ($gatunek == "4") {
-                        $gatslownie = "horror";
-                } else if ($gatunek == "5") {
-                    $gatslownie = "komedia";
-                }
-                
-
-
-
-                echo "<h2>".$gatunek.". ".$gatslownie."</h2>";
-
                 if ($res->num_rows > 0) {
                     while ($row = $res->fetch_assoc()) {
-                        echo "<p>tytul: ".$row['tytul']."; Rok produkcji: ".$row['rok']."; ocena: ".$row['ocena']."</p>";
+                        echo "<p>".$row['id'].". ".$row['tytul'].", reżyseria: ".$row["imie_rezysera"]." ".$row["nazwisko_rezysera"]."</p>";
                     }
                 } 
-
             } catch (Exception $e) {
                 echo "Error: " . $e->getMessage();
             }
