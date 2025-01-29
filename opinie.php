1af5ea78bf13a3499be061e3939310795e4f031b
Nikodem Okroj <n.okroj@icloud.com>
2025-01-29 12:53:42 +0100

    Dodano layout strony - bez stylow

--- opinie.php
+++ opinie.php
@@ -17,6 +17,30 @@ try {
     <link rel="stylesheet" href="./styl3.css"/>
 </head>
 <body>
-    <h1>здраствуй цие</h1>
+    <div class="container">
+        <div class="header">
+            <h1>Hurtowania Spożywcza</h1>
+        </div>
+        <div class="main">
+            <h2>Opinie naszych klientów</h2>
+        </div>
+        <div class="footer">
+            <div class="footer_1">
+                <h3>Współpracuj z nami</h3>
+                <a href="http://sklep.pl/">Sklep 1</a>
+            </div>
+            <div class="footer_2">
+                <h3>Nasi top klienc</h3>
+
+            </div>
+            <div class="footer_3">
+                <h3>Skontaktuj się</h3>
+                <p>Telefon: 111222333</p>
+            </div>
+            <div class="footer_4">
+                <h3>Autor: 00000000000</h3>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
