1a0b6c3e680607575ef69766cdc6b1f302b68ad7
Nikodem Okroj <n.okroj@icloud.com>
2025-01-29 12:47:32 +0100

    Podpieto arkusz stylow css

--- opinie.php
+++ opinie.php
@@ -9,11 +9,12 @@ try {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./styl3.css"/>
 </head>
 <body>
     <h1>здраствуй цие</h1>
