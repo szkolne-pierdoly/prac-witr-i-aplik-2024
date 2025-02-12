b76c824c6eb5c5d62fccb3e977d7a68205f1c6a7
Nikodem Okroj <n.okroj@icloud.com>
2025-02-12 12:29:18 +0100

    dodano pierwszy request

--- poziomRzek.php
+++ poziomRzek.php
@@ -1,11 +1,11 @@
 <?php
-try {
-    $connection = new mysqli("db", "admin", "supersecret", "baza", 3306);
-    if ($connection->connect_error) {
-        throw new Exception("Connection failed: " . $connection->connect_error);
-    }
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
+$pokaz = "wszystko";
+
+echo $_POST['pokaz'];
+
+if (isset($_POST['pokaz'])) {
+    $pokaz = $_POST['pokaz'];
+    echo $pokaz;
 }
 ?>
 <!DOCTYPE html>
@@ -41,18 +41,44 @@ try {
             <div class="left">
                 <h3>Stany na dzień 2022-05-05</h3>
                 <table class="table">
-                    <th class="t-header">
-                        <td>Wodomierz</td>
-                        <td>Rzeka</td>
-                        <td>Ostrzegawczy</td>
-                        <td>Alarmowy</td>
-                        <td>Aktualny</td>
-                    </th>
-                    <tr>
-                        <td>
-
-                        </td>
+                    <tr class="t-header">
+                        <th>Wodomierz</th>
+                        <th>Rzeka</th>
+                        <th>Ostrzegawczy</th>
+                        <th>Alarmowy</th>
+                        <th>Aktualny</th>
                     </tr>
+                    <?php
+                        $sqlAll = "SELECT wodowskazy.nazwa, wodowskazy.rzeka, MAX(pomiary.stanWody) AS stanWody, MAX(wodowskazy.stanOstrzegawczy) AS stanOstrzegawczy, MAX(wodowskazy.stanAlarmowy) AS stanAlarmowy FROM wodowskazy JOIN pomiary ON wodowskazy.id = pomiary.wodowskazy_id  WHERE pomiary.dataPomiaru = '2022-05-05' GROUP BY wodowskazy.rzeka, wodowskazy.nazwa;";
+                        $sqlPonadStan = "SELECT wodowskazy.nazwa, wodowskazy.rzeka, MAX(pomiary.stanWody) AS stanWody, MAX(wodowskazy.stanOstrzegawczy) AS stanOstrzegawczy, MAX(wodowskazy.stanAlarmowy) AS stanAlarmowy FROM wodowskazy JOIN pomiary ON wodowskazy.id = pomiary.wodowskazy_id  WHERE pomiary.dataPomiaru = '2022-05-05' AND pomiary.stanWody > wodowskazy.stanOstrzegawczy GROUP BY wodowskazy.rzeka, wodowskazy.nazwa;";
+                        $sqlOstrzenie = "SELECT wodowskazy.nazwa, wodowskazy.rzeka, MAX(pomiary.stanWody) AS stanWody, MAX(wodowskazy.stanOstrzegawczy) AS stanOstrzegawczy, MAX(wodowskazy.stanAlarmowy) AS stanAlarmowy FROM wodowskazy JOIN pomiary ON wodowskazy.id = pomiary.wodowskazy_id  WHERE pomiary.dataPomiaru = '2022-05-05' GROUP BY wodowskazy.rzeka, wodowskazy.nazwa;";
+
+                        try {
+                            $connection = new mysqli("db", "root", "supersecret", "baza-05-02-2025", 3306);
+                            if ($connection->connect_error) {
+                                throw new Exception("Connection failed: " . $connection->connect_error);
+                            }
+
+                            $res = $connection->query($pokaz == 'wszystko' ? $sqlAll : $sqlOstrzenie);
+                            if ($res->num_rows > 0) {
+                                while ($row = $res->fetch_assoc()) {
+                                    echo "
+                                        <tr>
+                                            <td>".$row['nazwa']."</td>
+                                            <td>".$row['rzeka']."</td>
+                                            <td>".$row['stanOstrzegawczy']."</td>
+                                            <td>".$row['stanAlarmowy']."</td>
+                                            <td>".$row['stanWody']."</td>
+                                        </tr>
+                                    ";
+                                }
+                            } else {
+                                echo "<td>Brak wynikow</td>";
+                            }
+                        } catch (Exception $e) {
+                            echo "Error: " . $e->getMessage();
+                        }
+                    ?>
                 </table>
 
             </div>
