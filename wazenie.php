9e5f8571ba3599a433c9e757e15c6998253e499f
Nikodem Okroj <n.okroj@icloud.com>
2025-03-05 12:55:59 +0100

    dodano style css oraz uklad strony

--- wazenie.php
+++ wazenie.php
@@ -7,6 +7,42 @@
     <link href="styl.css" rel="stylesheet"/>
 </head>
 <body>
-    <h1>здраствуй цие</h1>
+    <div class="baner">
+        <div class="baner__1">
+            <h1>Warzenie pojazdów we Wrocławiu</h1>
+        </div>
+        <div class="baner__2">
+            <img src="obraz1.png" alt="waga"/>
+        </div>
+    </div>
+    <div class="content">
+        <div class="lokalizacje">
+            <h2>Lokalizacje wag</h2>
+            <ol>
+                <!-- skrypt 1 -->
+            </ol>
+            <h2>Kontakt</h2>
+            <a href="mailto:wazenie@wroclaw.pl">Napisz</a>
+        </div>
+        <div class="alerty">
+            <h2>Alerty</h2>
+            <table>
+                <tr>
+                    <th>Rejestracja</th>
+                    <th>Ulica</th>
+                    <th>Waga</th>
+                    <th>Dzien</th>
+                    <th>Czas</th>
+                </tr>
+                <!-- skrypt 2 -->
+            </table>
+        </div>
+        <div class="image">
+            <img src="obraz2.png" alt="tir" />
+        </div>
+    </div>
+    <div class="footer">
+        <p>Strone wykonal: 00000000000</p>
+    </div>
 </body>
 </html>
