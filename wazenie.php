70493df0b9508e7443adbe1dba57698a6565ae4a
Nikodem Okroj <n.okroj@icloud.com>
2025-03-05 12:15:49 +0100

    dodano plik wazenie, usunieto index, dodano i podlaczone styl css

---
+++ wazenie.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ważenie samochodów ciężarowych</title>
+    <link href="styl.css" rel="stylesheet"/>
+</head>
+<body>
+    <h1>здраствуй цие</h1>
+</body>
+</html>
