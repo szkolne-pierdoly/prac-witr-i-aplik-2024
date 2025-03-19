bdab622d03cb10bd1dc79457e69ebf79a81a96af
Nikodem Okroj <n.okroj@icloud.com>
2025-03-19 12:42:00 +0100

    Dodano nowy plik kup-auto.php z podstawową strukturą HTML i formularzem do wyboru marki samochodu.

---
+++ kup-auto.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Komis aut</title>
+  <link href="./styl.css" rel="stylesheet"/>
+</head>
+<body>
+  <div class="contnet">
+    <header class="baner">
+      <h1><em>KupAuto!</em> Internetowy Komis Samochodowy</h1>
+    </header>
+    <main class="main">
+      <div class="main-1">
+
+      </div>
+      <div class="main-2">
+        <h2>Wybierz markę</h2>
+        <form method="post">
+          <select>
+            
+          </select>
+          <input type="submit" value="Wyszukaj" />
+        </form>
+      </div>
+      <div class="main-3">
+
+      </div>
+    </main>
+    <footer class="footer">
+      <p>Strone wykonał: +48 694202137</p>
+      <a href="http://firmy.pl/komis">Znajdź nas także</a>
+    </footer>
+  </div>
+</body>
+</html>
