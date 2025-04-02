d576cea1fff88a3a0f226a415ba15e5ade1e33ec
Nikodem Okroj <n.okroj@icloud.com>
2025-04-02 12:42:48 +0200

    Usunięto plik index.php i dodano nowy plik sklep.php z podstawową strukturą HTML dla internetowego sklepu z eko-warzywami.

---
+++ sklep.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ważywniak</title>
+    <meta charset="UTF8">
+    <link rel="stylesheet" href="./styl2.css" />
+</head>
+<body>
+    <div class="main">
+        <header class="both-baners">
+            <header class="baner1">
+                <h1>Internetowy sklep z eko-warzywami</h1>
+            </header>
+            <nav class="baner2">
+                <ol>
+                    <li>warzywa</li>
+                    <li>owoce</li>
+                    <li><a href="https://terapiasokami.pl">soki</a></li>
+                </ol>
+            </nav>
+        </header>
+        <main class="main">
+            
+        </main>
+        <footer class="footer">
+            <form>
+                <label>nazwa:</label>
+                <input type="text" />
+                <label>Cena</label>
+                <input type="text" />
+                <input type="submit" value="Dodaj produkt" />
+            </form>
+            <p>Strone wykonal: 11111111111</p>
+        </footer>
+    </div>
+</body>
+</html>
