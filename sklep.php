1f232f8fc010f5bf43b3acace07d5df118da44db
Nikodem Okroj <n.okroj@icloud.com>
2025-04-02 12:59:11 +0200

    dodano dzialajacy skrypt 1

--- sklep.php
+++ sklep.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="./styl2.css" />
 </head>
 <body>
-    <div class="main">
+    <div class="root">
         <header class="both-baners">
             <header class="baner1">
                 <h1>Internetowy sklep z eko-warzywami</h1>
@@ -22,7 +22,30 @@
             </nav>
         </header>
         <main class="main">
-            
+            <?php
+                $conn = mysqli_connect('db', 'root', 'supersecret', 'dane2'); // change to localhost if not using docker to run
+                if (mysqli_connect_errno()) {
+                    printf("", mysqli_connect_error());
+                    exit(1);
+                }
+                $sql = "SELECT nazwa, ilosc, opis, cena, zdjecie FROM produkty WHERE produkty.Rodzaje_id = (1 || 2);";
+
+                $response = mysqli_query($conn, $sql);
+                if ($response->num_rows) {
+                    while ($row = mysqli_fetch_assoc($response)) {
+                        echo '
+                        <div class="produkt">
+                            <img src="'.$row["zdjecie"].'" />
+                            <h5>'.$row['nazwa'].'</h5>
+                            <p>opis: '.$row['opis'].'</p>
+                            <p>na stanie: '.$row['ilosc'].'</p>
+                            <h2>'.$row['cena'].' zł</h2>
+                        </div>
+                        ';
+                    }
+                }
+
+            ?>
         </main>
         <footer class="footer">
             <form>
